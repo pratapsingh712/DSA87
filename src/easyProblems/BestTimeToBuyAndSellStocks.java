01e42a93534df65d95caf535d525100141f47fcb
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2025-09-22 00:23:30 +0530

    added brute force approach to get the maximum profit on stock sold

--- src/easyProblems/BestTimeToBuyAndSellStocks.java
+++ src/easyProblems/BestTimeToBuyAndSellStocks.java
@@ -3,5 +3,28 @@ package easyProblems;
 public class BestTimeToBuyAndSellStocks {
     public static void main(String[] args) {
 
+        int[] array = {7,1,5,3,6,4};
+
+        int maxProfit = Integer.MIN_VALUE;
+
+        int buyOn=-1;
+        int sellOn=-1;
+
+        for(int i=0;i<array.length;i++){
+            int buy = array[i];
+            for(int j=i+1;j<array.length;j++){
+                int sell = array[j];
+                int diff = sell-buy;
+                if(maxProfit<diff){
+                    maxProfit = diff;
+                    buyOn = i+1;
+                    sellOn = j+1;
+                }
+            }
+        }
+
+        System.out.println("Bought on day :"+buyOn);
+        System.out.println("Sold on day :"+sellOn);
+        System.out.println("Maximum profit made = "+maxProfit);
     }
 }
