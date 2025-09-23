bb3322741bb12804549f1b3b4dd9ce3279d9f40f
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2025-09-23 15:11:21 +0530

    added anagram check

---
+++ src/easyProblems/linkedList/Anagram.java
@@ -0,0 +1,32 @@
+package easyProblems.linkedList;
+
+public class Anagram {
+
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+
+        if(sortString(s).equals(sortString(t))){
+            System.out.println("They are Anagram of each other :"+true);
+        }else{
+            System.out.println("They are Anagram of each other :"+false);
+        }
+    }
+
+    private static String sortString(String s) {
+
+        char[] charArray = s.toCharArray();
+
+        for(int i=0;i<charArray.length;i++){
+            for(int j=i+1;j<charArray.length;j++){
+                if(charArray[i]>charArray[j]){
+                    char temp = charArray[i];
+                    charArray[i] = charArray[j];
+                    charArray[j] =  temp;
+                }
+            }
+        }
+
+        return new String(charArray);
+    }
+}
