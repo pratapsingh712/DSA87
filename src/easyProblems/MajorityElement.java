fd52f98e3b4b6fe15438528ad472a9f6444131e5
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2025-09-23 11:18:00 +0530

    added hashtable to get the majority element

---
+++ src/easyProblems/MajorityElement.java
@@ -0,0 +1,33 @@
+package easyProblems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        /*
+        I need to get the element that is appearing more tha half the length of array
+         */
+        int numbs[] = {2,2,1,1,1,2,2};
+
+        int appearedMoreThanNByTwo = numbs.length/2;
+
+        HashMap<Integer,Integer> counter = new HashMap<>();
+
+        for(int i=0;i<numbs.length;i++){
+            if(counter.containsKey(numbs[i])) {
+                counter.put(numbs[i], counter.get(numbs[i])+1);
+            }else{
+                counter.put(numbs[i],1);
+            }
+        }
+
+        for(Map.Entry<Integer,Integer> set : counter.entrySet()){
+            int num = set.getValue();
+            if(num>appearedMoreThanNByTwo){
+                System.out.println("Number that appeared more than half the length of array is : "+set.getKey());
+                break;
+            }
+        }
+    }
+}
