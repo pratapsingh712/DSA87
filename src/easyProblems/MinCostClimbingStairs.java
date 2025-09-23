e1ee6546a8604d83ed2c0faa79860596c0555e5c
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2025-09-24 04:34:45 +0530

    solved minimum cost climbing problem

---
+++ src/easyProblems/MinCostClimbingStairs.java
@@ -0,0 +1,24 @@
+package easyProblems;
+
+public class MinCostClimbingStairs {
+    public static void main(String[] args) {
+        int[] cost = {10,15,20};
+        int minCost = minCostClimbingStairs(cost);
+
+        System.out.println("Minimum cost to climb the stair would be : "+minCost);
+    }
+
+    public static int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n+1];
+
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for(int i=2;i<=n;i++){
+            dp[i] = Math.min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
+        }
+
+        return dp[n];
+    }
+}
